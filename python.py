195efd16228ea8d866bbdee722613be30e617699
BigGeF <52731933+BigGeF@users.noreply.github.com>
2020-10-12 22:03:15 -0700

    '说明'

--- python.py
+++ python.py
@@ -1,20 +1,20 @@
-# nums = [-2,1,-3,4,-1,2,1,-5,4]
+nums = [-2,1,-3,4,-1,2,1,-5,4]
 
-# def  maxSubArray(nums):
-#     if max(nums)<0:
-#         return max(nums)
+def  maxSubArray(nums):
+    if max(nums)<0:
+        return max(nums)
     
-#     local_max, global_max = 0 , 0
-#     for num in nums:
-#         local_max = max(0,local_max+num)
-#         global_max= max(global_max,local_max)
+    local_max, global_max = 0 , 0
+    for num in nums:
+        local_max = max(0,local_max+num)
+        global_max= max(global_max,local_max)
     
-#     return global_max
+    return global_max
 
-# print(maxSubArray(nums))
+print(maxSubArray(nums))
+print('HelloWorld')
 
-
-# 118 pascal's Triangle
+118 pascal's Triangle
 numsRows = 5
 
 def generate(rows):
