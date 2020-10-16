d94ead8b34fa1e11a9f942750e9f1db63143dba3
BigGeF <52731933+BigGeF@users.noreply.github.com>
2020-10-16 08:46:37 -0700

    oct16

--- python.py
+++ python.py
@@ -15,16 +15,30 @@
 # print('HelloWorld123')
 
 # 118 pascal's Triangle
-numsRows = 3
+# numsRows = 3
 
-def generate(rows):
-    result=[]
-    for i in range(rows):
-        result.append([])
-        for j in range(i+1):
-            if j in (0,i):
-                 result[i].append(1)
-            else:
-                result[i].append(result[i-1][j-1] + result[i-1][j])
-    return result
-print(generate(numsRows))
+# def generate(rows):
+#     result=[]
+#     for i in range(rows):
+#         result.append([])
+#         for j in range(i+1):
+#             if j in (0,i):
+#                  result[i].append(1)
+#             else:
+#                 result[i].append(result[i-1][j-1] + result[i-1][j])
+#     return result
+# print(generate(numsRows))
+
+# 119 pascal's triangle
+# numsRows = 3
+
+# def generate(rows):
+#     result = [1]+[0]*rows
+#     for i in range(rows):
+#         for j in range(i+1,0,-1):
+#             result[j]=result[j]+result[j-1]
+    
+#     return result
+# print(generate(numsRows))
+for i in range(5+1,0,-1):
+    print(i)
